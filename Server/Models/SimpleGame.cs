86cea6ce29ae224a9154e9dd6841083093d07964
Zunzap <chlo1259@gmail.com>
2015-01-12 07:46:32 +0100

    Update New Model

---
+++ Server/Models/SimpleGame.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Server.Models
+{
+    public class SimpleGame
+    {
+        public String Player1Name { get; set; }
+        public int Player1Score { get; set; }
+
+        public String Player2Name { get; set; }
+        public int Player2Score { get; set; }
+    }
+}
