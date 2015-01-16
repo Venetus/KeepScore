05636df637b7fe0ac3d4c783ee562a7a65d6f170
Chropez <chlo1259@gmail.com>
2015-01-16 18:17:03 +0100

    Test commit from linux

--- Client/app/adapters/application.js
+++ Client/app/adapters/application.js
@@ -1,6 +1,8 @@
 import webApiAdapter from './web-api';
 
 export default webApiAdapter.extend({
-	host: 'http://localhost:57907/',
+	/*host: 'http://localhost:57907',*/
+	host: 'http://10.0.2.2:57907',
 	namespace: 'api'
 });
+ 
